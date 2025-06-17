ab6e75a8948d490250c3348ad22462407bb5aa9e
Calum Gunn <47633906+calum-gunn1@users.noreply.github.com>
2025-06-17 13:22:32 +0100

    Update publish-tests.sh

--- bin/publish-tests.sh
+++ bin/publish-tests.sh
@@ -1,30 +1,15 @@
 #!/bin/sh
 
-ALLURE_DIRECTORY="$PWD/allure-report"
-ACCESSIBILITY_DIRECTORY="$PWD/reports"
+DIRECTORY="$PWD/allure-report"
 
 echo "Publishing test results to S3"
 
 if [ -n "$RESULTS_OUTPUT_S3_PATH" ]; then
-   # Publish Allure reports
-   if [ -d "$ALLURE_DIRECTORY" ]; then
-      aws s3 cp --quiet "$ALLURE_DIRECTORY" "$RESULTS_OUTPUT_S3_PATH/allure-report" --recursive
-      echo "Allure reports published to $RESULTS_OUTPUT_S3_PATH/allure-report"
+   if [ -d "$DIRECTORY" ]; then
+      aws s3 cp --quiet "$DIRECTORY" "$RESULTS_OUTPUT_S3_PATH" --recursive
+      echo "Test results published to $RESULTS_OUTPUT_S3_PATH"
    else
-      echo "$ALLURE_DIRECTORY is not found"
-   fi
-
-   # Publish accessibility reports
-   if [ -d "$ACCESSIBILITY_DIRECTORY" ]; then
-      aws s3 cp --quiet "$ACCESSIBILITY_DIRECTORY" "$RESULTS_OUTPUT_S3_PATH/accessibility-reports" --recursive
-      echo "Accessibility reports published to $RESULTS_OUTPUT_S3_PATH/accessibility-reports"
-   else
-      echo "$ACCESSIBILITY_DIRECTORY is not found"
-   fi
-
-   # Check if at least one directory was found
-   if [ ! -d "$ALLURE_DIRECTORY" ] && [ ! -d "$ACCESSIBILITY_DIRECTORY" ]; then
-      echo "No test reports found to publish"
+      echo "$DIRECTORY is not found"
       exit 1
    fi
 else
