5060b5d48fbcad3d5e600f56dc4db7c4c23a1d54
Calum Gunn <47633906+calum-gunn1@users.noreply.github.com>
2025-10-01 15:16:01 +0100

    Update home.e2e.js

--- test/specs/home.e2e.js
+++ test/specs/home.e2e.js
@@ -66,7 +66,7 @@ describe('Home page', () => {
   })
 
   it('should display RAG Status Definitions section', async () => {
-    const ragStatusHeading = await $('h3*=RAG status definitions')
+    const ragStatusHeading = await $('h2*=RAG status definitions')
     await expect(ragStatusHeading).toBeDisplayed()
 
     // Check for specific RAG status definitions
